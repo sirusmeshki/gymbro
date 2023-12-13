234c690aa62bf60d2512e8c98fb558a4ccd88015
AmirHossein <amirhosssein.jfr@gmail.com>
2023-12-13 19:14:28 +0330

    refactor: useToggle instead of useState

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { FC, useState } from 'react'
+import { FC } from 'react'
 
 import Image from 'next/image'
 
 import clsx from 'clsx'
+import { useToggle } from 'usehooks-ts'
 
 type AccordionProps = {
     title: string
@@ -12,22 +13,23 @@ type AccordionProps = {
 }
 
 const Accordion: FC<AccordionProps> = ({ title, children }) => {
-    const [isOpen, setIsOpen] = useState(true)
+    // Open and close accordion
+    const [isOpen, toggle] = useToggle()
 
     return (
-        <li className='border-bottom'>
+        <li className="border-bottom">
             <header
-                className='flex cursor-pointer items-center justify-between gap-1 px-5 py-8'
-                onClick={() => setIsOpen((prev) => !prev)}>
-                <h5 className='text-base font-bold text-neutral-800'>
+                className="flex cursor-pointer items-center justify-between gap-1 px-5 py-8"
+                onClick={() => toggle()}>
+                <h5 className="text-base font-bold text-neutral-800">
                     {title}
                 </h5>
                 <Image
                     className={clsx(isOpen && 'rotate-180')}
                     width={24}
                     height={24}
-                    src='/icon/arrow.svg'
-                    alt='arrow icon'
+                    src="/icon/arrow.svg"
+                    alt="arrow icon"
                 />
             </header>
             <div
@@ -35,7 +37,7 @@ const Accordion: FC<AccordionProps> = ({ title, children }) => {
                     isOpen ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]',
                     'grid transition-all'
                 )}>
-                <div className='overflow-hidden text-xs font-normal leading-5 text-neutral-400 sm:text-sm'>
+                <div className="overflow-hidden text-xs font-normal leading-5 text-neutral-400 sm:text-sm">
                     {children}
                 </div>
             </div>
