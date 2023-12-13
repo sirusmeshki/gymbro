170675bdfbc232bb22d9c02eba5180ec5b7944db
AmirHossein <amirhosssein.jfr@gmail.com>
2023-12-13 18:12:24 +0330

    refactor: deleted useless files

--- src/components/HOC/Container.tsx
+++
@@ -1,9 +0,0 @@
-// const Container = ({ children }: { children: JSX.Element }) => {
-//   return (
-//     <section className="w-full h-full flex border-bottom pb-20 sm:pb-24 md:pb-32 lg:pb-0">
-//       {children}
-//     </section>
-//   );
-// };
-
-// export default Container;
