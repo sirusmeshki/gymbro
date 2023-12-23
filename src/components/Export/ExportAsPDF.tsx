166b2f3b416e31c18f73660624189f6f6ca9df32
AmirHossein <amirhosssein.jfr@gmail.com>
2023-12-23 17:28:23 +0330

    fixed: export pdf

--- src/components/Export/ExportAsPDF.tsx
+++ src/components/Export/ExportAsPDF.tsx
@@ -4,38 +4,34 @@ import Image from 'next/image'
 
 import Button from '../Button'
 
-import { usePDF } from '@react-pdf/renderer'
 import PDF from '@/app/_pdf/page'
-import dynamic from 'next/dynamic'
 
-const Template = dynamic(() => import('@/app/_pdf/page'), {
-    loading: () => <p>Loading...</p>,
-    ssr: false,
-})
+import { useIsClient } from 'usehooks-ts'
+import { PDFDownloadLink } from '@react-pdf/renderer'
 
 const ExportAsPDF = () => {
-    const [instance, updateInstance] = usePDF({ document: <PDF /> })
-    if (instance.loading) return <div>Loading ...</div>
-    if (instance.error) return <div>Something went wrong: {instance.error}</div>
-
-    let url = instance.url as string
+    const isClient = useIsClient()
 
     return (
-        <a href={url} download="gymbro.pdf">
-            <Button
-                className="border-bottom max-h-24 min-h-[96px] w-full"
-                cta="اکسپورت PDF"
-                placeholder={
-                    <Image
-                        src="/icon/arrow-left.svg"
-                        alt="arrow left"
-                        width={24}
-                        height={24}
+        <>
+            {isClient && (
+                <PDFDownloadLink document={<PDF />} fileName="gymbro.pdf">
+                    <Button
+                        className="border-bottom max-h-24 min-h-[96px] w-full"
+                        cta="اکسپورت PDF"
+                        placeholder={
+                            <Image
+                                src="/icon/arrow-left.svg"
+                                alt="arrow left"
+                                width={24}
+                                height={24}
+                            />
+                        }
+                        alt="submit workouts button"
                     />
-                }
-                alt="submit workouts button"
-            />
-        </a>
+                </PDFDownloadLink>
+            )}
+        </>
     )
 }
 
