b06e26fa8f22551327955025b363abb223465c22
AmirHossein <sirusmeshki@gmail.com>
2024-01-15 23:32:47 +0330

    chore: workouts sidebar header commented

--- src/layout/Sidebars/WorkoutSidebar.tsx
+++ src/layout/Sidebars/WorkoutSidebar.tsx
@@ -5,12 +5,14 @@ import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { useLocalStorage } from 'usehooks-ts'
+
 import Button from '@/components/Button'
-import Workouts from '@/components/Cards/Workouts'
+// import Workouts from '@/'
 import Selectbox from '@/components/Input/Selectbox'
+import Workouts from '@/components/Cards/Workouts/Move'
 
 const WorkoutSidebar = () => {
-    const [activeSession, setActiveSession] = useState('1')
     const [sessions] = useState([
         { value: '1' },
         { value: '2' },
@@ -20,19 +22,21 @@ const WorkoutSidebar = () => {
         { value: '6' },
     ])
 
+    const [session, setSession] = useLocalStorage('session', '1')
+
     return (
         <aside className="absolute flex min-w-[480px] translate-x-full flex-col lg:fixed lg:h-[calc(100dvh-42px)] lg:translate-x-0">
-            <header className="w-[calc(100vw - 10px)] border-bottom flex min-h-[96px] items-center justify-center">
+            {/* <header className="w-[calc(100vw - 10px)] border-bottom flex min-h-[96px] items-center justify-center">
                 <Selectbox
                     className="h-full w-2/4"
-                    title="جلسه ها"
+                    title={`جلسه ها =${session}`}
                     options={sessions}
-                    onOptionClick={(e) => setActiveSession(e)}
-                    activeOption={activeSession}
+                    onOptionClick={(e) => setSession(e)}
+                    activeOption={session}
                 />
                 <Button
-                    cta="ایجاد تغییرات"
                     className="bg-lightPurple"
+                    cta="ایجاد تغییرات"
                     placeholder={
                         <Image
                             src="/icon/arrow-left.svg"
@@ -43,7 +47,7 @@ const WorkoutSidebar = () => {
                     }
                     alt="edit workouts button"
                 />
-            </header>
+            </header> */}
             <Workouts />
             <Link href="/plan" className="border-top mb-44 h-32">
                 <Button
